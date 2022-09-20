682f89d805e5e05d7eb3592748aa127634895bc2
Wendy You <wyou130@gmail.com>
2022-09-20 18:12:57 -0400

    commit tree 102e62e3889377599f9831b160c68f472488eb1e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        # self.title.include?("Won't believe" "Secret" "Top" "Guess")
+        errors.add(:title, "must include clickbait") unless self.title.include? "Won't Believe"
+        # self.title["Won't believe", "Secret", "Top", "Guess"]
+        # self.title["Won't believe"]
+    end
+
 end
